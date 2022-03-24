b90f56d7848a7846225dd0b33e0a117f2921fccf
hritt95 <78231863+hritt95@users.noreply.github.com>
2022-03-24 19:57:55 -0400

    fixed the tallys now they work

--- tally.js
+++ tally.js
@@ -1,12 +1,41 @@
-let value = 0; 
+let clicks;
 function setup(){
     createCanvas(displayWidth, displayHeight);
+    background(169, 209, 212);  
+    clicks = 0;
+    
   }
-  function draw(){
-      //Background color
+  function mouseClicked(){
+    clicks +=1;
+    console.log("clicks", clicks);
+    let lineCount = 0;
+    stroke (28,128,128);
+    strokeWeight(5);
+    for(let y = 40; y <height-40; y += 40){
+      for(let x = 50; x < 350; x += 10){
+        if(lineCount<clicks){
+          lineCount+=1
+          console.log("lineCounts",lineCount);
+          if(lineCount % 5 == 0){
+            console.log("group5");
+            line(x-50,y+20,x,y)
+          }else{
+            console.log("nonGroup");
+            line(x,y,x,y+20)
+          }
+        }
+      }
+    }
+  }
+  function keyPressed(){
+    clear();
+    clicks = 0;
     background(169, 209, 212);    
-    //Tally mark colors 
-    stroke (255,215,0);
-    strokeWeight(2);
+  }
+  
+  function draw(){
+   
+    
+   
   }
   
